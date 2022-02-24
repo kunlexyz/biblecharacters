b17d179eaf838417855deaf9c83723381c294717
kunlexyz <bobbykay007@gmail.com>
2022-02-25 00:09:19 +0100

    checking https or http

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include('index.html'); ?>
+<?php 
+
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+echo 'it is http';
+/*
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+*/
+exit;
+}
+
+
+echo 'it is httpsssss';
+exit;
+//include('index.html'); 
+
+?>
