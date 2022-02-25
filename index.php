51e795f4a790078a576d1a6f356057b256518afa
kunlexyz <bobbykay007@gmail.com>
2022-02-25 14:08:43 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php 
-/*
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-echo 'it is http';
-exit;
 
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-
-}
-
-
-echo 'it is httpsssss';
-exit;
-*/
-echo '<br><br>server name =';
-echo $_SERVER['SERVER_NAME'];
-
-echo '<br><br>php-self =';
-echo $_SERVER['PHP_SELF'];
-
-echo '<br><br>http_host =';
-echo $_SERVER['HTTP_HOST'];
-
-exit;
-//include('index.html'); 
+include('index.html'); 
 
 ?>
